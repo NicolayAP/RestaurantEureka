81ef70bc1074c4c96bc5702fafa453ac5015d019
WBOK-GM <rellenoa2@gmail.com>
2025-10-23 08:06:56 -0500

    fronted

---
+++ food-delivery-app/src/app/restaurant-listing/components/restaurant-listing.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { Restaurant } from 'src/app/Shared/models/Restaurant';
+import { Router } from '@angular/router';
+import { RestaurantService } from '../service/restaurant.service';
+
+@Component({
+  selector: 'app-restaurant-listing',
+  templateUrl: './restaurant-listing.component.html',
+  styleUrls: ['./restaurant-listing.component.css']
+})
+export class RestaurantListingComponent {
+
+  public restaurantList: Restaurant[];
+  public ramdonImageUrl: string[] = [];
+
+  ngOnInit() {
+    this.getAllRestaurants();
+  }
+
+  constructor(private router: Router, private restaurantService: RestaurantService) {}
+
+  getAllRestaurants(){ 
+    this.restaurantService.getAllRestaurants().subscribe(
+      data => {
+      this.restaurantList = data;
+      this.restaurantList.forEach(restaurant => {
+        this.ramdonImageUrl.push('assets/restaurant-pics/' + this.getRadomImage());
+      });
+    });
+  }
+
+  getRamdonNumber(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  getRadomImage(): string {
+    const imageCount = 8; 
+    const ramdonNum = this.getRamdonNumber(1, imageCount);
+    return `${ramdonNum}.jpg`;
+  }
+
+  onButtonClick(id: number){
+    this.router.navigate(['/food-catalogo', id]);
+  }
+}
---
+++ food-delivery-app/src/app/restaurant-listing/service/restaurant.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
+import { API_URL_RL } from "../../constants/url";
+
+@Injectable({
+  providedIn: "root"
+})
+export class RestaurantService {
+  private apiUrl = API_URL_RL+"/restaurants/fetchAllRestaurants";
+  constructor(private http: HttpClient) {}
+
+  getRestaurants(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}`).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+    private handleError(error: any) {
+    console.error('An error occurred:', error);
+    return throwError(() => error.message || 'error');
+    }
+}
